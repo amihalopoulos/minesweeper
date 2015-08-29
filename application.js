491011592ae3bdbf9475cc4c6f438b621b8a10b9
Alexander Mihalopoulos <amihalop@gmail.com>
2015-08-29 18:28:54 -0400

    game working. time to refactor

--- application.js
+++ application.js
@@ -5,57 +5,12 @@ $(document).ready(function() {
   $('body').on('mousedown', '.cell', function(event) {
     switch (event.which) {
       case 1:
-        // press(this)
         game.click(this.id)
-        $('#container').html(game.render())
         break;
       case 3:
         game.flag(this.id)
-        $('#container').html(game.render())
         break;
     }
+    $('#container').html(game.render())
   });
 });
-
-var press = function(pressed) {
-  if (game.objectBoard[pressed.id[0]][pressed.id[1]]["bomb"]){
-    $(pressed).css("background-image", bombImage)
-    alert("KABBOOOYYYYYAAAAA")
-    location.reload();
-  }
-  else if (game.getSurroundingBombCount(pressed.id) == 0){
-    addClickClass(pressed)
-    var neighbors = game.getNeighbors(pressed.id)
-    for (var i = 0; i < neighbors.length; i++) {
-      var n = neighbors[i].join('')
-      console.log(n)
-      press(document.getElementById(n))
-    };
-  }
-  else {
-    addClickClass(pressed)
-  }
-}
-
-var addBombClass = function(cell) {
-  $(cell).removeClass("unclicked flagged")
-  $(cell).addClass("bomb")
-}
-
-var addClickClass = function(cell) {
-  $(cell).removeClass("unclicked flagged")
-  $(cell).addClass("clicked")
-  $(cell).css("background-color", "#bbb")
-  $(cell).html(game.getSurroundingBombCount(cell.id))
-}
-
-var addFlagClass = function(cell){
-  if ($(cell).hasClass('unclicked')) {
-    $(cell).addClass("flagged")
-  }
-}
-var flagImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyD6AtiHqeH_sAfNNvTr1xq4xtTPOY7AYa2YD7C3gFtK9fLf1C')"
-
-var bombImage = "url('http://image.shutterstock.com/display_pic_with_logo/1222298/134991800/stock-vector-comic-book-bomb-explosion-vector-illustration-134991800.jpg')"
-
-
--- game.js
+++ game.js
@@ -4,103 +4,123 @@ function Game(size) {
   this.board = placeBombs(this.size)
   this.objectBoard = this.splitBoardIntoRows()
 }
-
-Game.prototype.splitBoardIntoRows = function() {
-  var nestArray = []
-  var hashBoard = hashify(this.board)
-  while (hashBoard.length > 0) {
-    row = hashBoard.splice(0,Math.sqrt(this.size))
-    nestArray.push(row)
-  }
-  return nestArray
-}
-
-Game.prototype.render = function() {
-  var string = "<table id='board'>"
-  if (this.gameOver === false) {
-    for (var x=0; x<this.objectBoard.length; x++){
-      string += "<tr class='row'>"
-      for (var y=0; y<this.objectBoard[x].length;y++){
-        string += "<td class='cell "
-        if (this.objectBoard[x][y]["status"] === "unclicked"){
-          string += "unclicked' id ='" + x + '' + y + "'>"
-        } else if (this.objectBoard[x][y]["status"] === "clicked") {
-          string += "clicked' id ='" + x + '' + y + "'>" + this.getSurroundingBombCount([x,y])
-        } else if (this.objectBoard[x][y]["status"] === "flagged"){
-          string += "flagged' id ='" + x + '' + y + "'>"
-        }
-        string += "</td>"
+Game.prototype = {
+  constructor: Game,
+  render: function() {
+    var string = "<table id='board'>"
+    if (this.gameOver === false) {
+      for (var x=0; x<this.objectBoard.length; x++){
+        string += "<tr class='row'>"
+        for (var y=0; y<this.objectBoard[x].length;y++){
+          string += "<td class='cell "
+          if (this.objectBoard[x][y]["status"] === "unclicked"){
+            string += "unclicked' id ='" + x + '' + y + "'>"
+          } else if (this.objectBoard[x][y]["status"] === "clicked") {
+            string += "clicked' id ='" + x + '' + y + "'>" + this.getSurroundingBombCount([x,y])
+          } else if (this.objectBoard[x][y]["status"] === "flagged"){
+            string += "flagged' id ='" + x + '' + y + "'>"
+          }
+          string += "</td>"
+        };
+        string += "</tr>"
       };
-      string += "</tr>"
+    } else {
+      for (var x=0; x<this.objectBoard.length; x++){
+        string += "<tr class='row'>"
+        for (var y=0; y<this.objectBoard[x].length;y++){
+          string += "<td class='cell "
+          if (this.objectBoard[x][y]["bomb"]){
+            string += "bomb' id ='" + x + '' + y + "'>"
+          } else if (this.objectBoard[x][y]["status"] === "clicked" || this.objectBoard[x][y]["status"] === "unclicked") {
+            string += "clicked' id ='" + x + '' + y + "'>" + this.getSurroundingBombCount([x,y])
+          } else if (this.objectBoard[x][y]["status"] === "flagged"){
+            string += "flagged' id ='" + x + '' + y + "'>"
+          }
+          string += "</td>"
+        };
+        string += "</tr>"
+      };
     };
-  } else {
-    string = "Game over, bitch"
-  };
-  string += "</table>"
-  console.log(string)
-  return string
-}
-
-Game.prototype.click = function(cell){
-  x = cell[0]
-  y = cell[1]
-  var self = this
-  this.objectBoard[x][y]["status"] = "clicked"
-  if (this.objectBoard[x][y]["bomb"]) {
-    this.gameOver = true
-    //show all bombs, alert, refresh
-  } else if (this.getSurroundingBombCount(cell) === 0){
-    console.log(this.getNeighbors(cell))
-    // var n = this.getNeighbors(cell)
-    // n.forEach(function(coord){ self.click(coord)})
-  } else {
-    console.log("there are bombs nearby")
+    string += "</table>"
+    return string
+  },
+  click: function(cell) {
+    x = cell[0]
+    y = cell[1]
+    var self = this
+    this.objectBoard[x][y]["status"] = "clicked"
+    if (this.objectBoard[x][y]["bomb"]) {
+      this.gameOver = true
+      //show all bombs, alert, refresh
+    } else if (self.getSurroundingBombCount(cell) === 0){
+      console.log(this.getNeighbors(cell))
+      this.getNeighbors(cell).forEach(function(n) {self.click(n)})
+    };
+  },
+  flag: function(cell) {
+    x = cell[0]
+    y = cell[1]
+    this.objectBoard[x][y]["status"] = "flagged"
+  },
+  getSurroundingBombCount: function(cell) {
+    var board = this.objectBoard
+    var x = parseInt(cell[0])
+    var y = parseInt(cell[1])
+    var count = 0
+    if (this.inBounds(x+1, y) && board[x+1][y]["bomb"]) {
+      count+=1
+    }
+    if (this.inBounds(x+1, y-1) && board[x+1][y-1]["bomb"]) {
+      count+=1
+    }
+    if (this.inBounds(x+1, y+1) && board[x+1][y+1]["bomb"]) {
+      count+=1
+    }
+    if (this.inBounds(x, y+1) && board[x][y+1]["bomb"]) {
+      count+=1
+    }
+    if (this.inBounds(x, y-1) && board[x][y-1]["bomb"]) {
+      count+=1
+    }
+    if (this.inBounds(x-1, y) && board[x-1][y]["bomb"]) {
+      count+=1
+    }
+    if (this.inBounds(x-1, y+1) && board[x-1][y+1]["bomb"]) {
+      count+=1
+    }
+    if (this.inBounds(x-1, y-1) && board[x-1][y-1]["bomb"]) {
+      count+=1
+    }
+    return count
+  },
+  inBounds: function(x,y) {
+    if (x < Math.sqrt(this.size) && x >= 0 && y < Math.sqrt(this.size) && y >= 0) {
+      return true
+    } else {return false}
+  },
+  getNeighbors: function(index) {
+    var x = parseInt(index[0])
+    var y = parseInt(index[1])
+    var directions = [[1,0], [1,1], [0,1], [-1,1], [-1, 0], [-1,-1], [0, -1], [1,-1]]
+    var neighbors = []
+    for (var i = 0; i < directions.length; i++) {
+      var neighborX = x + directions[i][0]
+      var neighborY = y +directions[i][1]
+      if (this.inBounds(neighborX, neighborY) && !this.objectBoard[neighborX][neighborY]["bomb"] && this.objectBoard[neighborX][neighborY]["status"] === "unclicked"){
+        neighbors.push([neighborX, neighborY])
+      }
+    };
+    return neighbors
+  },
+  splitBoardIntoRows: function() {
+    var nestArray = []
+    var hashBoard = hashify(this.board)
+    while (hashBoard.length > 0) {
+      row = hashBoard.splice(0,Math.sqrt(this.size))
+      nestArray.push(row)
+    }
+    return nestArray
   }
-  console.log("clicked" + cell)
-}
-
-Game.prototype.flag = function(cell){
-  x = cell[0]
-  y = cell[1]
-  this.objectBoard[x][y]["status"] = "flagged"
-}
-
-Game.prototype.getSurroundingBombCount = function(cell) {
-  var board = this.objectBoard
-  var x = parseInt(cell[0])
-  var y = parseInt(cell[1])
-  var count = 0
-  if (this.inBounds(x+1, y) && board[x+1][y]["bomb"]) {
-    count+=1
-  }
-  if (this.inBounds(x+1, y-1) && board[x+1][y-1]["bomb"]) {
-    count+=1
-  }
-  if (this.inBounds(x+1, y+1) && board[x+1][y+1]["bomb"]) {
-    count+=1
-  }
-  if (this.inBounds(x, y+1) && board[x][y+1]["bomb"]) {
-    count+=1
-  }
-  if (this.inBounds(x, y-1) && board[x][y-1]["bomb"]) {
-    count+=1
-  }
-  if (this.inBounds(x-1, y) && board[x-1][y]["bomb"]) {
-    count+=1
-  }
-  if (this.inBounds(x-1, y+1) && board[x-1][y+1]["bomb"]) {
-    count+=1
-  }
-  if (this.inBounds(x-1, y-1) && board[x-1][y-1]["bomb"]) {
-    count+=1
-  }
-  return count
-}
-
-Game.prototype.inBounds = function(x,y) {
-  if (x < Math.sqrt(this.size) && x >= 0 && y < Math.sqrt(this.size) && y >= 0) {
-    return true
-  } else {return false}
 }
 
 var placeBombs = function(num) {
@@ -124,23 +144,4 @@ var hashify = function(board) {
     }
   };
   return containerArray
-}
-
-Game.prototype.getNeighbors = function(index) {
-  var x = parseInt(index[0])
-  var y = parseInt(index[1])
-  var directions = [[1,0], [1,1], [0,1], [-1,1], [-1, 0], [-1,-1], [0, -1], [1,-1]]
-  var neighbors = []
-  for (var i = 0; i < directions.length; i++) {
-    var neighborX = x + directions[i][0]
-    var neighborY = y +directions[i][1]
-    if (this.inBounds(neighborX, neighborY) && !this.objectBoard[neighborX][neighborY]["bomb"]){
-      neighbors.push([neighborX, neighborY])
-    }
-  };
-  return neighbors
-}
-
-Game.prototype.clickNeighbors = function(index) {
-
 }
